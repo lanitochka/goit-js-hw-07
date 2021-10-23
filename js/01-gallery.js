b1daca0db6ec99327270b8f6431aaac23ccfefef
lanitochka <70783335+lanitochka@users.noreply.github.com>
2021-10-23 15:57:22 +0200

    initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,13 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()

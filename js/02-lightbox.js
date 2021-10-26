312dc7165ccd7f903f3c44c25b3be0907e12eab5
lanitochka <70783335+lanitochka@users.noreply.github.com>
2021-10-26 23:39:58 +0200

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `
+    })
+    
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
